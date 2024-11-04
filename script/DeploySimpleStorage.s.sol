d7ba641b9feca80615d5276261dfd75716536f7d
Eajun <eajunooi@gmail.com>
2024-11-04 21:49:51 +0800

    Update path

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
-import {SimpleStorage} from "../src/SimpleStorage.sol";
+import {SimpleStorage} from "../src/Simple-Storage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
