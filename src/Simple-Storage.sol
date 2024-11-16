05720464d7fa82a559eeeaf16f15026547ed0bdd
Eajun <eajunooi@gmail.com>
2024-11-16 13:52:57 +0800

    Update formatting

--- src/Simple-Storage.sol
+++ src/Simple-Storage.sol
@@ -1,10 +1,6 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
-
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
     uint256 myFavoriteNumber;
@@ -13,9 +9,8 @@ contract SimpleStorage {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
-    Person[] public listOfPeople;
 
+    Person[] public listOfPeople;
     mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
